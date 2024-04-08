7bd7ce907a9dfff47c8ec039f656fceb383f9a04
sergii-nosachenko <sergii.n@mate.academy>
2024-04-08 14:07:42 +0300

    Trigger

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const $ = require('jquery');
 const addButtonsToReviews = require('./functions/addButtonsToReviews');
 const GHPage = require('./classes/GHPage.class');
+const showReviewsAdded = require('./functions/showReviewsAdded');
 
 $(() => {
   const pageUrl = GHPage.linkToPage();
   console.log('Current page:', pageUrl);
 
   switch (true) {
-    case /\/pull\//.test(pageUrl):
+    case /\/mate-academy\/[^/]+\/pull\//.test(pageUrl):
       addButtonsToReviews();
       break;
+
+    case /\/mate-academy\/[^/]+\/pulls/.test(pageUrl):
+      showReviewsAdded();
+      break;
+
     default:
       console.log('Skipping the page.');
   }
