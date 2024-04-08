05f8d6b0d46dcfa6ee43480f58587005af9d3037
sergii-nosachenko <sergii.n@mate.academy>
2024-04-08 13:04:09 +0300

    Config updated

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
             warnings: false,
             comparisons: false,
             inline: 2,
-            drop_console: true,
+            drop_console: false,
             drop_debugger: true,
             pure_funcs: ['console.info', 'console.debug', 'console.warn'],
           },
